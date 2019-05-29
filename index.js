7a2e2879f7c176c5463124600d51c0572e60c8f5
Arron M <marshall.arron@gmail.com>
2019-05-29 09:10:03 -0700

    Set up initial project on local

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code away!
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const logger = (req, res, next) => {
+  // logger middleware
+  next();
+}
+
+const validateUserId = (req, res, next) => {
+  // validate userid middleware
+  next();
+}
+
+const validateUser = (req, res, next) => {
+  // validate user middleware
+  next();
+}
+
+const validatePost = (req, res, next) => {
+  // validate post middleware
+  next();
+}
+
+
+// GET /api/users
+
+// POST /api/users
+
+// DELETE /api/users/id
+
+// PUT /api/users/:id
