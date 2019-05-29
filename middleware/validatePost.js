c698526a5b20b55431dd0343e8c045e70d3c5dd4
Arron M <marshall.arron@gmail.com>
2019-05-29 14:12:49 -0700

    Create validatePost middleware

--- middleware/validatePost.js
+++ middleware/validatePost.js
@@ -1,5 +1,12 @@
 const validatePost = (req, res, next) => {
-  
+  if (Object.keys(req.body).length == 0) return res.status(400).json({
+    message: 'missing post data'
+  });
+
+  if (!req.body.text) return res.status(400).json({
+    message: 'missing required text field'
+  });
+
   next();
 }
 
